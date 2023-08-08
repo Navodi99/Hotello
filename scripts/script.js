b0da773da9a9511e363ecf59bb898c5fd5b23a97
Navodi99 <navodil99@gmail.com>
2023-08-08 15:40:35 +0530

    Home page was completed

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+// const carousel = document.querySelector(".carousel");
+// const images = carousel.getElementsByTagName("img");
+// let currentIndex = 0;
+
+// function showImage(index) {
+//   for (let i = 0; i < images.length; i++) {
+//     images[i].style.display = "none";
+//   }
+//   images[index].style.display = "block";
+//   currentIndex = index;
+// }
+
+// function changeImage(direction) {
+//   currentIndex += direction;
+//   if (currentIndex < 0) {
+//     currentIndex = images.length - 1;
+//   } else if (currentIndex >= images.length) {
+//     currentIndex = 0;
+//   }
+//   showImage(currentIndex);
+// }
+
+// showImage(currentIndex);
